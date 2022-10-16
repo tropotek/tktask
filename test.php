90a13ec8743104e156a146c1d6672ece4455c29c
tropotek <info@tropotek.com>
2022-10-16 14:02:43 +1000

    ~Auto: Commit

--- test.php
+++ test.php
@@ -2,33 +2,3 @@
 require_once __DIR__ . '/_prepend.php';
 
 echo 'SUCCESS!!';
-
-
-
-//dump($factory->getDb()->getTableInfo('_session'));
-//dump($factory->getDb()->getTableInfo('config'));
-//dump($factory->getDb()->getTableInfo('user'));
-
-
-//dump($system->getComposer());
-
-
-/*
-
-$array = [
-    'one' => [
-        'test'
-    ]
-];
-$array['two'][][] = ['Another Test'];
-dump($array);
-
-
-$src = ['one' => 'vone', 'two' => 'vtwo', 'three' => 'vthree'];
-$add = ['zero' => 'vzero'];
-dump(array_merge($add, $src));
-dump($add + $src);
-*/
-
-
-
