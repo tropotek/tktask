fb516417996fcc45dcac771ac9268476993f0616
tropotek <info@tropotek.com>
2022-10-22 07:13:15 +1000

    ~Auto: Commit

--- src/config/routes.php
+++ src/config/routes.php
@@ -8,20 +8,19 @@ use Symfony\Component\Routing;
 
 $routes = \Tk\Factory::instance()->getRouteCollection();
 
-$routes->add('home',      new Routing\Route('/',     ['_controller' => '\App\Controller\Home::doDefault']));
-$routes->add('home-path', new Routing\Route('/home', ['_controller' => '\App\Controller\Home::doDefault']));
-$routes->add('install',   new Routing\Route('/install',     ['_controller' => '\App\Controller\Install::doDefault']));
+$routes->add('home',      new Routing\Route('/',        ['_controller' => '\App\Controller\Home::doDefault']));
+$routes->add('home-path', new Routing\Route('/home',    ['_controller' => '\App\Controller\Home::doDefault']));
+$routes->add('install',   new Routing\Route('/install', ['_controller' => '\App\Controller\Install::doDefault']));
 
-$routes->add('phpinfo',   new Routing\Route('/info', ['_controller' => '\App\Controller\Info::doDefault']));
-$routes->add('dom-test',  new Routing\Route('/domTest',     ['_controller' => '\App\Controller\DomTest::doDefault']));
+// Authentication pages
+$routes->add('login',     new Routing\Route('/login',   ['_controller' => '\App\Controller\Login::doDefault']));
+$routes->add('logout',    new Routing\Route('/logout',  ['_controller' => '\App\Controller\Login::doLogout']));
 
-
-$routes->add('user-login',     new Routing\Route('/login',     ['_controller' => '\App\Controller\Login::doDefault']));
-$routes->add('user-login',     new Routing\Route('/login',     ['_controller' => '\App\Controller\Login::doDefault']));
-$routes->add('user-logout',    new Routing\Route('/logout',    ['_controller' => '\App\Controller\Login::doLogout']));
-
+// Site user Pages
 $routes->add('user-dashboard', new Routing\Route('/dashboard',      ['_controller' => '\App\Controller\Dashboard::doDefault']));
 $routes->add('user-manager',   new Routing\Route('/userManager',    ['_controller' => '\App\Controller\User\Manager::doLogout']));
 $routes->add('user-edit',      new Routing\Route('/userEdit/{id}',  ['_controller' => '\App\Controller\UserEdit::doLogout']));
 
-
+// Test pages
+$routes->add('phpinfo',   new Routing\Route('/info',    ['_controller' => '\App\Controller\Info::doDefault']));
+$routes->add('dom-test',  new Routing\Route('/domTest', ['_controller' => '\App\Controller\DomTest::doDefault']));
