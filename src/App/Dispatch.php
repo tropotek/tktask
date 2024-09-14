5ee8c08f2e6f8e931c77e9345c88a6f9ba53243c
tropotek <info@tropotek.com>
2024-09-14 14:27:51 +1000

    Move event listeners

--- src/App/Dispatch.php
+++ src/App/Dispatch.php
@@ -8,31 +8,25 @@ class Dispatch extends \Bs\Dispatch
     /**
      * Any Common listeners that are used in both HTTPS or CLI requests
      */
-    protected function commonInit()
+    protected function commonInit(): void
     {
         parent::commonInit();
-
-
     }
 
     /**
      * Called this when executing http requests
      */
-    protected function httpInit()
+    protected function httpInit(): void
     {
         parent::httpInit();
-
-
     }
 
     /**
      * Called this when executing Console/CLI requests
      */
-    protected function cliInit()
+    protected function cliInit(): void
     {
         parent::cliInit();
-
-
     }
 
 }
