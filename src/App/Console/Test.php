de7c65b98d6aa52a0dd119937ee735daee0e7265
tropotek <info@tropotek.com>
2025-04-18 09:34:20 +1000

    ~Auto: Commit

--- src/App/Console/Test.php
+++ src/App/Console/Test.php
@@ -6,6 +6,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Bs\Console\Console;
 use Tk\Config;
+use Tk\Uri;
 
 class Test extends Console
 {
@@ -22,6 +23,37 @@ class Test extends Console
             $this->writeError('Error: Only run this command in a debug environment.');
             return self::FAILURE;
         }
+
+        //$url = Uri::create('http://php.net');
+        //$url = Uri::create('https://tropotek.com.au');
+        $url = Uri::create('https://godar.ttek.org/Projects/tktask/tkping');
+        $opts = [
+            'http' => [
+                //'header' => 'Cookie: '.$_SERVER['HTTP_COOKIE']."\r\n",
+                'method' => 'HEAD'
+            ],
+            'ssl' => ['verify_peer' => false, 'verify_peer_name' => false],
+        ];
+        $context = stream_context_create($opts);
+        $data = file_get_contents($url, false, $context);
+        if ($data === false) {
+            vd('404 Not Found');
+        } else {
+            vd($data, json_encode($data));
+        }
+
+//        $fd = fopen($url, 'rb', false, $context);
+//        if ($fd === false) {
+//            vd('404 Not Found');
+//        } else {
+//            vd(stream_get_meta_data($fd));
+//            fclose($fd);
+//        }
+
+
+
+
+
 
         return self::SUCCESS;
     }
